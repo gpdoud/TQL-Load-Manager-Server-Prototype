4dd30299ceeb908e14bb93c4d54b67850b522735
Matthew Luedtke <MLuedtke@tql.com>
2021-07-19 15:17:48 -0400

    basic models with basic props

---
+++ LoadManagerPrototype/Models/Load.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadManagerPrototype.Models
+{
+    public class Load
+    {
+        public int Id { get; set; }
+        public int PONumber { get; set; }
+        public Customer Customer { get; set; }
+        public Shed PickShed { get; set; }
+        public Shed DropShed { get; set; }
+        public Carrier Carrier { get; set; }
+        public User User { get; set; }
+        public decimal PayTruckRate { get; set; }
+        public decimal CustomerBillRate { get; set; }
+
+    }
+    
+}
