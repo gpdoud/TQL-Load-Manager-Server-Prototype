4da707a3e2cf77ade19905b87af2c31455851cdd
Charlie Traylor <ctraylor@tql.com>
2021-07-19 15:29:35 -0400

    added LmDbContext

--- LoadManagerPrototype/Data/LmDbContext.cs
+++ LoadManagerPrototype/Data/LmDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LoadManagerPrototype.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,13 @@ namespace LoadManagerPrototype.Data
 {
     public class LmDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        public LmDbContext(DbContextOptions<LmDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder bulider)
+        {
+
+        }
     }
 }
