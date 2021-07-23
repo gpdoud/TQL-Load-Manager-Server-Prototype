0e7ea40e677b8f3cb7cc5a1e39aa0f4df642a11d
Matthew Luedtke <MLuedtke@tql.com>
2021-07-23 12:34:48 -0400

    bug fix

---
+++ LoadManagerPrototype/Migrations/20210723163323_status.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoadManagerPrototype.Migrations
+{
+    public partial class status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Load",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Load");
+        }
+    }
+}
